384a4e56db6952a606c375e286b54d05b54fc86e
Emil Madsen <emil@magenta.dk>
2025-03-11 16:43:17 +0100

    test(pytest_util): [#64131] add mypy 'test' for retry
    This commit adds a freestanding function inside the test-suite to
    trigger a mypy type error during pre-commit to prove that our retry
    wrapper is poorly typed.

--- tests/fastramqpi/test_pytest_util.py
+++ tests/fastramqpi/test_pytest_util.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
+import re
 import time
+from typing import Callable
 from typing import NoReturn
 
 import pytest
@@ -22,3 +24,13 @@ def test_retry() -> None:
     end_time = time.monotonic()
 
     assert 2 <= (end_time - start_time) <= 3
+
+
+# This function is here to ensure that @retry does not ruin our function typing, i.e.
+# to ensure that mypy is happy with the return-type of our function.
+def retry_typing() -> Callable[[int], bool]:
+    @retry()
+    def is_even(x: int) -> bool:
+        return bool(re.search(r"[02468]$", str(x)))
+
+    return is_even
