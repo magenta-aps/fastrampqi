7890244e3cfb7c53cd153a426fc4e4f0b21468ce
Emil Madsen <emil@magenta.dk>
2025-08-18 00:06:41 +0200

    test(pytest_plugin): [#64762] add tests for run_server asynccontextmanager
    This commit introduces tests for the `run_server` asynccontextmanager to
    ensure that it behaves correctly, however as it does not currently
    behave as we desire one of the added test-cases are marked as`xfail`'ed,
    which will be resolved in the following commit.

---
+++ tests/fastramqpi/test_pytest_run_server.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from contextlib import suppress
+
+import pytest
+from fastapi import FastAPI
+from httpx import ConnectError
+
+from fastramqpi.pytest_plugin import run_server
+from fastramqpi.pytest_plugin import run_test_client
+
+# Test FastAPI "Hello World" app
+app = FastAPI()
+
+
+@app.get("/")
+def index() -> str:
+    return "Hello World"
+
+
+async def check_http_request() -> None:
+    async with run_test_client() as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.text == '"Hello World"'
+
+
+async def test_run_server() -> None:
+    # Before the server started our HTTP call fails
+    with pytest.raises(ConnectError) as exc_info:
+        await check_http_request()
+    assert "All connection attempts failed" in str(exc_info.value)
+
+    # When the server is running our HTTP calls succeed
+    async with run_server(app):
+        await check_http_request()
+
+    # After the server shutdown our HTTP call fails
+    with pytest.raises(ConnectError) as exc_info:
+        await check_http_request()
+    assert "All connection attempts failed" in str(exc_info.value)
+
+
+async def test_run_server_restarts() -> None:
+    # Do multiple startups and shutdowns checking our request works each time
+    for _ in range(3):
+        async with run_server(app):
+            await check_http_request()
+
+
+@pytest.mark.xfail(
+    reason="""
+    The server does not shutdown when an exception occurs, rather it keeps running,
+    meaning the second run_server call fails due to the port already being taken.
+    """
+)
+async def test_run_server_exception() -> None:
+    with suppress(ValueError):
+        # Server should shutdown gracefully even if an exception occurs
+        async with run_server(app):
+            await check_http_request()
+            raise ValueError("BOOM")
+
+    # After the server shutdown our HTTP call should fail
+    # If it does not, it means the server is still running
+    with pytest.raises(ConnectError) as exc_info:
+        await check_http_request()
+    assert "All connection attempts failed" in str(exc_info.value)
+
+    # Even if a previous server run failed, we should be able to restart
+    async with run_server(app):
+        await check_http_request()
